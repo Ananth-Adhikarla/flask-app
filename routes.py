8ad072851927ae05081cdfd4e25177de3d909075
ananth.adhikarla@gmail.com <ananth.adhikarla@gmail.com>
2018-02-23 17:01:19 +0530

    routes.py change to true

--- routes.py
+++ routes.py
@@ -4,8 +4,8 @@ from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///learningflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db.init_app(app)
 
 app.secret_key = "development-key"
